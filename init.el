8261c6d46b2cada54d126fce847a277c31d00d2f
Byung-Hyun You <zolamusic@users.noreply.github.com>
2018-01-25 15:55:12 +0900

    Update init.el

--- init.el
+++ init.el
@@ -22,7 +22,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (flycheck-julia julia-repl julia-mode intero auctex hlinum flycheck-clojure clojure-mode-extra-font-locking cider clojure-mode yaml-mode markdown-mode rtags flycheck-rtags ivy-rtags company-rtags cmake-ide cmake-mode haskell-mode rainbow-delimiters smartparens srefactor nyan-mode neotree magit company flycheck ivy smex doom-themes))))
+    (flycheck-julia julia-repl julia-mode intero auctex hlinum flycheck-clojure clojure-mode-extra-font-locking cider clojure-mode yaml-mode markdown-mode rtags flycheck-rtags ivy-rtags company-rtags cmake-ide cmake-mode haskell-mode rainbow-delimiters smartparens srefactor nyan-mode neotree magit company flycheck ivy smex))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
