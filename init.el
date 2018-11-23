1cfe2341156edb6704d4233375ee2fbd0cf6f353
Byung-Hyun You <lambda@FlyingMushroom.local>
2018-11-23 15:21:14 +0900

    Adjusting proive init line.

--- init.el
+++ init.el
@@ -15,8 +15,6 @@
 ;(load-file "~/.emacs.d/config/latex-mode.el")
 ;(load-file "~/.emacs.d/config/julia-mode.el")
 
-(provide 'init)
-;;; init.el ends here
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -37,3 +35,6 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+(provide 'init)
+;;; init.el ends here
