c50482158ff61f0fa1140fa8724df9590d4291c4
Byung-Hyun You <zolamusic@users.noreply.github.com>
2019-04-13 00:07:15 +0900

    Update global-mode.el

--- config/global-mode.el
+++ config/global-mode.el
@@ -25,10 +25,10 @@
 ;;  :init
 ;;  (load-theme 'one-dark t))
 
-(use-package base16-theme
-  :ensure t
-  :config
-  (load-theme 'base16-onedark t))
+;;(use-package base16-theme
+;;  :ensure t
+;;  :config
+;;  (load-theme 'base16-onedark t))
 
 ;;(use-package doom-themes
 ;;  :ensure t
@@ -55,7 +55,7 @@
   :bind ("M-8" . neotree-toggle)
   :config
   (setq neo-theme 'ascii)
-  (setq neo-smart-open t)
+;;  (setq neo-smart-open t)
   (setq neo-window-fixed-size nil)
   (setq-default neo-show-hidden-files nil))
 
@@ -72,7 +72,7 @@
 (use-package yaml-mode
   :ensure t)
 
-(setq base16-theme-256-color-source "base16-shell")
+;;(setq base16-theme-256-color-source "base16-shell")
 (add-to-list 'auto-mode-alist '("\\.yaml\\'" . yaml-mode))
 
 (add-hook 'prog-mode-hook 'linum-mode)
@@ -82,7 +82,7 @@
 
 (menu-bar-mode -1)
 (tool-bar-mode -1)
-(scroll-bar-mode -1)
+;;(scroll-bar-mode -1)
 
 (defalias 'yes-or-no-p 'y-or-n-p)
 
@@ -96,22 +96,22 @@
 
 (setq make-backup-files nil)
 
-(set-language-environment "Korean")
+;;(set-language-environment "Korean")
 (prefer-coding-system 'utf-8)
-(global-set-key (kbd "<S-kana>") 'toggle-input-method)
+;;(global-set-key (kbd "<S-kana>") 'toggle-input-method)
 
 (global-set-key (kbd "M-<left>") 'shrink-window-horizontally)
 (global-set-key (kbd "M-<right>") 'enlarge-window-horizontally)
 (global-set-key (kbd "M-<down>") 'shrink-window)
 (global-set-key (kbd "M-<up>") 'enlarge-window)
 
-(set-face-attribute 'default nil :family "Hack Nerd Font")
+;(set-face-attribute 'default nil :family "Hack Nerd Font")
 ;(set-face-attribute 'default nil :height 120)
 ;(set-fontset-font t 'hangul (font-spec :name "Neodunggeunmo")) ;; 한글폰트조아
 ;(setq default-frame-alist '((font . "Neodunggeunmo-12")))
 
 ;(set-face-bold 'bold nil)
-(add-to-list 'exec-path "/usr/local/bin/")
+;(add-to-list 'exec-path "/usr/local/bin/")
 
 (setq comint-prompt-read-only t)
 
